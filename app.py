9da86b1b43dd2dec59d1c5ead75217e3e61c3acf
Markuss E <ezergailis.markuss@gmail.com>
2026-09-20 08:41:38 +0200

    feat: updated the data sorting

--- app.py
+++ app.py
@@ -17,11 +17,49 @@ response = requests.get(url, headers=request_info)
 
 if response.status_code == 200:
     data = response.json()
-    print(data)
 
-    #USED FOR: saving the information into a txt file
-    #with open("test.txt", "w") as f:
-        #f.write(str(data))
+    #The actual product information
+    product = data["product"]
+
+    #basic information
+    print("----- PRODUCT-----")
+    print(f"Name: {product.get('product_name', 'Unknown')}")
+    print(f"Brand: {product.get('brands', 'Unknown')}")
+    print(f"Barcode: {product.get('code', barcode)}")
+
+    #cateogries
+    print("-----Categories-----")
+
+    main_category = product.get("pnns_groups_1", "Unknown")
+    sub_category = product.get("pnns_groups_2", "Unknown")
+
+    #detailed categories
+    print("-----Detailed Categories-----")
+
+    categories = product.get("categories_hierarchy", [])
+
+    for category in categories:
+    # Remove the language prefix, e.g. "en:fruit-juices"
+        category_name = category.split(":", 1)[-1]
+
+        # Make it easier to read
+        category_name = category_name.replace("-", " ").title()
+
+        print(f"- {category_name}")
+
+    #nutirition facts
+    nutriments = product.get("nutriments", {})
+
+    print("-----Nutrients-----")
+
+    print("Calories:", nutriments.get("energy-kcal_100g"))
+    print("Protein:", nutriments.get("proteins_100g"))
+    print("Carbs:", nutriments.get("carbohydrates_100g"))
+    print("Sugar:", nutriments.get("sugars_100g"))
+    print("Fat:", nutriments.get("fat_100g"))
+    print("Saturated fat:", nutriments.get("saturated-fat_100g"))
+    print("Fiber:", nutriments.get("fiber_100g"))
+    print("Salt:", nutriments.get("salt_100g"))
 
 else:
     print(f"Something went wrong. Status code: {response.status_code}")
--- info.md
+++ info.md
@@ -5,4 +5,8 @@ This is the first day of the project. Here's the discoveries about the project t
         It would accept it as input and return back all the product details, example is here: "https://world.openfoodfacts.org/api/v2/product/4000177026717 " -> This a barcode for a caprisun. 
     - So far the identified most useful information:
         Product Name, Brand, Service Size, Product quantitiy, Cateogry, Calories, Protein, Carbohydrates, Sugar, Fat, Saturated Fat, Fiber, Salt.
-    
+
+20/09/2026
+    - Managed to get the proper prints working, now it does actually display all of the nutrition information outright in the print statements. So far this has been tested on only 2 products. Caprisun Jungle Safari("4000177026717") and Driend Mango Slices("7090046314103")
+    - Soon need to start making a front end, the goal is to host it on varcel to have some sort of accessability from the mobile device, but the logic is not completed today...
+    - Self note: Much like in university, I NEED to start making a plan before I actually start implomenting. Otherwise the scope creep will overtake this entire project.
